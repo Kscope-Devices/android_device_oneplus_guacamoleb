9eacb0aa8003ed1f561cc7146cedd7fc7920e9cb
TH779 <i@779.moe>
2022-01-10 14:33:51 +0800

    guacamoleb: Kaleidoscope is a dream
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I51f90f863bf303c66daf861a79a68376dba6e33b

--- kscope_guacamoleb.mk
+++ kscope_guacamoleb.mk
@@ -11,14 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from guacamoleb device
 $(call inherit-product, device/oneplus/guacamoleb/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Kaleidoscope stuff.
+$(call inherit-product, vendor/kscope/target/product/mobile.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_guacamoleb
+PRODUCT_NAME := kscope_guacamoleb
 PRODUCT_DEVICE := guacamoleb
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := GM1905
+PRODUCT_MODEL := GM1900
 PRODUCT_MANUFACTURER := OnePlus
 
 PRODUCT_SYSTEM_NAME := OnePlus7
@@ -29,8 +29,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2240
-TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Build info
 BUILD_FINGERPRINT := "OnePlus/OnePlus7/OnePlus7:10/QKQ1.190716.003/2003131900:user/release-keys"
